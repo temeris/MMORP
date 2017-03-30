3eb2a844dcefe82a727f047009a0ca3ef3024037
Timothée <tim.bm@hotmail.fr>
2017-03-30 11:48:34 +0200

    Add files via upload

--- RPG Tim/app.js
+++ RPG Tim/app.js
@@ -5,13 +5,39 @@ var express = require('express'),
 
 app.use(express.static(__dirname + '/'));
 
-io.sockets.on('connection', function(socket,player){
-	socket.on('nouveau_player', function(player) {
-        socket.player = player;
-        //~ socket.broadcast.emit('nouveau_client', pseudo);
-    });
-	socket.on('mouvement',function(player1){
-		socket.broadcast.emit('mouvement', player1);
+function Player()
+{
+	this.x;
+	this.y;
+	this.oldx;
+	this.oldy;
+	this.newx;
+	this.newy;
+	this.name;
+};
+
+io.sockets.on('connection', function(socket){
+	var player = new Player();
+	socket.emit('lancement', player);
+	socket.on('reponse',function(player,ctx,canvas){
+		socket.player = player;
+		socket.ctx = ctx;
+		socket.canvas = canvas;
+	});
+	socket.on('init_position_depart',function(player){
+		player.x = 10;
+		player.y = 10;
+		socket.player = player;
+		socket.emit('ma_position',player);
+		socket.broadcast.emit('position_depart_players',player);
+	});
+	socket.on('action',function(data){
+		player.x = data.x;
+		player.y = data.y;
+		socket.player = player;
+		socket.emit('mouvement_pour_moi', player);
+		socket.broadcast.emit('mouvement_pour_autres', player);
+		io.sockets.clients().forEach(function (socket) { .. });
 	});
 });
 
