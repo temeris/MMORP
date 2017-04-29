94529a72ba12c46e16f6ff3cc91028edd55a0c3a
Timothée <tim.bm@hotmail.fr>
2017-04-29 11:33:41 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -2,16 +2,53 @@ var express = require('express'),
     app = express(),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server);
-
+    
 app.use(express.static(__dirname + '/'));
 
-io.sockets.on('connection', function(socket,player){
-	socket.on('nouveau_player', function(player) {
-        socket.player = player;
-        //~ socket.broadcast.emit('nouveau_client', pseudo);
-    });
-	socket.on('mouvement',function(player1){
-		socket.broadcast.emit('mouvement', player1);
+var modules = require("./gameplay.js");
+
+var map = new modules.Map();
+
+var allClients = [];
+var tires = [];
+
+io.sockets.on('connection', function(socket){
+	allClients.push(socket);
+	var player = modules.placerPlayer(map);
+	
+	socket.emit('lancement',{map,player});
+	
+	socket.on('reponse',function(playerL){
+		player = playerL;
+		socket.player = player;
+		io.emit('update_screen',player);
+	});
+	
+	socket.on('move',function(data){
+		player = data.player;
+		socket.player = player;
+		map = data.map;
+		for(i=0;i<40;i++)
+		{
+			for(j=0;j<20;j++)
+			{
+				if(map[i][j] != 0)console.log('serveur : i:',i,'j:',j);
+			}
+		}
+		io.emit('update_data',map);
+		io.emit('update_screen',player);
+	});
+	
+	socket.on('send_update',function(){
+		socket.emit('update_my_position',socket.player);
+		socket.broadcast.emit('update_my_position_for_others',socket.player);
+	});
+	
+	socket.on('disconnect', function(){
+		socket.broadcast.emit('delete_player',map,socket.player);
+		map[socket.player.x][socket.player.y]=0;
+		var i = allClients.indexOf(socket);
+		allClients.splice(i,1);
 	});
 });
 
---
+++ gameplay.js
@@ -0,0 +1,110 @@
+var exports = module.exports = {};
+
+module.exports.Map = function()
+{
+	var map = new Array;
+	for(i=0;i<40;i++)
+	{
+		map[i]=new Array;
+	}
+	for(i=0;i<40;i++)
+	{
+		for(j=0;j<20;j++)
+		{
+			map[i][j]=0;
+		}
+	}
+	return map;
+}
+
+module.exports.placerPlayer = function(map)
+{
+	if(map)
+	{
+		do
+		{
+			var x = Math.floor(Math.random()*40);
+			var y = Math.floor(Math.random()*20);
+		}
+		while(map[x][y] != 0);
+		var player = new Player(x,y);
+		map[x][y] = 1;
+		return player;
+	}
+}
+
+function Player(x,y)
+{
+	this.x=x;
+	this.y=y;
+	this.oldx=x;
+	this.oldy=y;
+	this.direction=2; // ~ left:0; top:1; right:2; bottom:3;
+	this.health=3;
+	this.name="";
+};
+
+function Immunity()
+{
+	var immunities = new Array();
+}
+
+function drawPlayer(player){
+	ctx.beginPath();
+	ctx.arc(20*player.x + 10, 20*player.y + 10, 10, 0, 2*(Math.PI));
+	ctx.fill();
+	ctx.fillStyle="black";
+	ctx.stroke();
+	ctx.beginPath();
+	if(player.direction == 0)
+	{
+		ctx.arc(20*player.x + 4, 20*player.y + 10, 4, 0, 2*(Math.PI));
+	}
+	else if(player.direction == 1)
+	{
+		ctx.arc(20*player.x + 10, 20*player.y + 4, 4, 0, 2*(Math.PI));
+	}
+	else if(player.direction == 2) 
+	{
+		ctx.arc(20*player.x + 16, 20*player.y + 10, 4, 0, 2*(Math.PI));
+	}
+	else if(player.direction == 3)
+	{
+		ctx.arc(20*player.x + 10, 20*player.y + 16, 4, 0, 2*(Math.PI));
+	}
+	ctx.fill();
+};
+	
+function createTire(data)
+{
+	var tire = new Tire(player.x,player.y,direction);
+	moveTire();
+	
+}
+
+function Tire(x,y,direction)
+{
+	this.x=x;
+	this.y=y;
+	this.direction = direction;
+}
+
+function moveTire()
+{
+	return;
+}
+
+//~ canvas.onclick = function(event)
+//~ {
+	//~ var data = getMousePos(canvas,event);
+	//~ createTire(data);
+//~ }
+
+//~ function getMousePos(canvas, event)
+//~ {
+	//~ var rect = canvas.getBoundingClientRect();
+	//~ return {
+		//~ x: event.clientX - rect.left,
+		//~ y: event.clientY - rect.top
+	//~ };
+//~ }
